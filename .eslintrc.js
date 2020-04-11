1c6ff1086a12e55046cdc075b039ea4ba64962d0
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-04-11 14:49:43 +0300

    refactor: remove empty line from the end of the .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "linebreak-style" : ["error", "windows"]
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "linebreak-style": ["error", "windows"],
+  },
 };
